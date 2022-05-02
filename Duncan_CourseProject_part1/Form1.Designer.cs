9c56647e21f3d9905542fe9b15e257bd1012e1aa
JeremyDuncan <jeremy.duncan1984@gmail.co>
2022-05-02 09:29:15 -0400

    Added button to form with label "Add Song".

--- Duncan_CourseProject_part1/Form1.Designer.cs
+++ Duncan_CourseProject_part1/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -121,11 +122,21 @@
             this.label5.TabIndex = 9;
             this.label5.Text = "URL";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 511);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(322, 95);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Add Song";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1922, 818);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -155,6 +166,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
     }
 }
 
